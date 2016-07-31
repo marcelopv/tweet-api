35186acfdc700815120d7a2814b0d7c4916d0a3e
Marcelo Vargas <marcelo.vargas90@gmail.com>
2016-07-31 10:34:39 -0300

    using strong parameters.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,8 +1,7 @@
 class TweetsController < ApplicationController
 
   def create
-    @tweet = Tweet.new
-    @tweet.text = params[:tweet][:text]
+    @tweet = Tweet.new(tweet_params)
     @tweet.save
 
     render json: 'Tweet created successfuly.'.to_json , content_type: "application/json", status: 200
@@ -15,10 +14,11 @@ class TweetsController < ApplicationController
   def update
     @tweet = Tweet.find(params[:id])
 
-    @tweet.text = params[:text]
-    @tweet.save
-
-    render json: 'Tweet updated successfuly.'.to_json , status: 200
+    if(@tweet.update(tweet_params))
+      render json: 'Tweet updated successfuly.'.to_json , status: 200
+    else
+      render json: 'Tweet was not updated successfuly.'.to_json , status: 200
+    end
   end
 
   def destroy
@@ -27,4 +27,10 @@ class TweetsController < ApplicationController
 
     render json: 'Tweet deleted successfuly.'.to_json , status: 200
   end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:text)
+  end
+
 end
